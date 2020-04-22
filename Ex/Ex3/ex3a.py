415b77a5a47be0cd4ae7ca22d049f9a0f0a87e59
Nadav Weisler <32937058+nadavWeisler@users.noreply.github.com>
2020-04-22 20:02:48 +0300

    ex3 Init

---
+++ Ex/Ex3/ex3a.py
@@ -0,0 +1,9 @@
+class Maze(object):
+
+    def __init__(self, csv_file_path):
+        pass
+
+    def maze_solver(self):
+        """ This method should use recursive methodology"""
+
+        return
---
+++ Ex/Ex3/exp3b.py
@@ -0,0 +1,24 @@
+SNAKES = [(27, 7), (36, 3), (50, 34), (59, 46), (66, 24), (73, 12), (76, 63), (89, 67), (97, 86), (99, 26)]
+LADDERS = [(2, 23), (7, 29), (22, 41), (28, 77), (30, 32), (44, 58), (54, 69), (70, 90), (80, 83),
+                        (87, 93)]
+
+class SnakesAndLadders(object):
+
+    def __init__(self, snakes_list, ladder_list):
+        self.snakes =  snakes_list
+        self.ladders = ladder_list
+        self.probs = [1/6,1/6,1/6,1/6,1/6,1/6]
+        pass
+
+    def update_dice(self, prob_list):
+        pass
+
+    def roll_dice(self):
+        return
+
+    def play_game(self):
+        return
+
+# if you want to initiate an instance of a game you can run this line:
+# game = SnakesAndLadders(SNAKES, LADDERS)
+# We defined SNAKES and LADDERS at the beginning of the file, so you could play with it
