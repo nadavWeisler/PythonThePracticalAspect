d609be0ffd3ceee7b26d6b8c037ca876d7d83649
Nadav Weisler <32937058+nadavWeisler@users.noreply.github.com>
2020-06-07 14:21:34 +0300

    ex5 init

---
+++ Ex/Ex5/ex5-sol/ex5a.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+# Part A
+# 1
+def filter_numeric_column(df, col_name, num1, num2):
+    pass
+
+# 2
+def switch_col_names(df, col_name1, col_name2):
+    pass
+
+# 3
+def double_special_letters(df, col_name_letter, col_name_value, col_name_results):
+    pass
+
+# 4
+def put_row_items_together(df, col_name_results):
+    pass
+
---
+++ Ex/Ex5/ex5-sol/ex5b.py
@@ -0,0 +1,10 @@
+# question 2
+def create_season_rating(csv_folder):
+    pass
+
+
+def most_lines_in_the_most_popular_episode(csv_folder):
+    pass
+
+def who_said_it(csv_folder, character_name, phrase):
+    pass
